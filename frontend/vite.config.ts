ffe7783bb2b39c8173fec711bb59424fc9df1f3d
Charlie Greenman <charlie.greenman1@gmail.com>
2024-09-16 11:49:35 -0400

    add relative path in vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   resolve: { alias: { './runtimeConfig': './runtimeConfig.browser' } },
   plugins: [
     react(),
