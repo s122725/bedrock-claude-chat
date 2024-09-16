38dcde5af4587c81e6424160d2e12976414cd4da
Charlie Greenman <charlie.greenman1@gmail.com>
2024-09-16 13:54:10 -0400

    update base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '',
   resolve: { alias: { './runtimeConfig': './runtimeConfig.browser' } },
   plugins: [
     react(),
